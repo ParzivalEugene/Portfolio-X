86d3bb23bf891f781da2bfa35a2e3d2529e141ce
Michkov Eugene <michkov.evgeny@gmail.com>
2023-07-23 18:43:01 +0300

    webpack fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 "use client";
-import { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 
 const grotesk = Space_Grotesk({ subsets: ["latin"] });
-
 
 export default function RootLayout({
   children,
@@ -17,10 +15,9 @@ export default function RootLayout({
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <title>Michkoff | Portfolio</title>
       </head>
-      <body className={grotesk.className}>
-        {children}
-      </body>
+      <body className={grotesk.className}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,42 +2,35 @@
 import FutureCard from "@/components/FutureCard";
 import Microchip from "@/components/Microchip";
 import WarningLine from "@/components/WarningLine";
-import { Metadata } from "next";
 import Image from "next/image";
-
-export const metadata: Metadata = {
-  title: "Michkoff | Portfolio",
-  description: "My professional portfolio",
-};
-
 
 export default function Home() {
   return (
-    <main className="flex h-full w-full items-center justify-center">
-      <div className="relative">
-        <div className="absolute -left-4 -top-4">
-          <Image src="/corner.svg" alt="corner" width={150} height={150} />
-        </div>
-        <div className="absolute -left-4 -bottom-4 -rotate-90">
-          <Image src="/corner.svg" alt="corner" width={150} height={150} />
-        </div>
-        <div className="absolute -right-4 -top-4 rotate-90">
-          <Image src="/corner.svg" alt="corner" width={150} height={150} />
-        </div>
-        <div className="absolute -right-4 -bottom-4 rotate-180">
-          <Image src="/corner.svg" alt="corner" width={150} height={150} />
-        </div>
-        <WarningLine />
-        <div className="grid grid-cols-main gap-24 py-12 h-fit items-center content-center">
-          <FutureCard />
-          <div className="flex flex-row gap-16">
-            <Image src="/m_logo.svg" alt="m_logo" width={300} height={300} />
-            <Image src="/e_logo.svg" alt="e_logo" width={300} height={300} />
+      <main className="flex h-full w-full items-center justify-center">
+        <div className="relative">
+          <div className="absolute -left-4 -top-4">
+            <Image src="/corner.svg" alt="corner" width={150} height={150} />
           </div>
-          <Microchip />
+          <div className="absolute -bottom-4 -left-4 -rotate-90">
+            <Image src="/corner.svg" alt="corner" width={150} height={150} />
+          </div>
+          <div className="absolute -right-4 -top-4 rotate-90">
+            <Image src="/corner.svg" alt="corner" width={150} height={150} />
+          </div>
+          <div className="absolute -bottom-4 -right-4 rotate-180">
+            <Image src="/corner.svg" alt="corner" width={150} height={150} />
+          </div>
+          <WarningLine />
+          <div className="grid h-fit grid-cols-main content-center items-center gap-24 py-12">
+            <FutureCard />
+            <div className="flex flex-row gap-16">
+              <Image src="/m_logo.svg" alt="m_logo" width={300} height={300} />
+              <Image src="/e_logo.svg" alt="e_logo" width={300} height={300} />
+            </div>
+            <Microchip />
+          </div>
+          <WarningLine />
         </div>
-        <WarningLine />
-      </div>
-    </main>
+      </main>
   );
 }
